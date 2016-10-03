c65bd01f1bd2b116fa3e607da90f539173fdd596
Syaefudhin <syaefudhin.9sn@gmail.com>
2016-10-03 21:39:40 +0700

    pseudocode  ax^2+bx+c=0 # CodeBloks#

---
+++ pseudocode.c
@@ -0,0 +1,25 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+
+float a,b,c,x1,x2;
+ int D;
+ D=pow(b,2)-4*a*c;
+if (D<0){
+    scanf("x1=x2 ,bilangan imajiner");
+    printf ("bilangan imajiner");
+}
+else if (D>0){
+    scanf(" x1=(-b+(sqrt pow(b,2)-4*a*c))/(2*a)");
+    scanf( "x2=(-b-(sqrt pow(b,2)-4*a*c))/(2*a)");
+    printf (" output x1 dan x2 ");
+}
+
+{
+printf("x1,x2");
+return 0;
+}
+}
+
